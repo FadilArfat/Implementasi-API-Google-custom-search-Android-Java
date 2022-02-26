a13e7fd0765228bd0e1c40a5e50c2de721bc757c
Muhammad Fadilah Arfat <fadilitas14@gmail.com>
2022-02-26 12:21:00 +0700

    first commit

---
+++ app/src/main/java/com/nfid/hoax/MainActivity.java
@@ -0,0 +1,18 @@
+package com.nfid.hoax;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.os.Bundle;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
